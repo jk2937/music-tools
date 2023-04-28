f2035a23f24e0abf8d3a15f4c56d4e40dcb86382
jk2937 <kaschakjonathan+github@gmail.com>
2023-04-28 01:46:52 -0400

    Modified music_module.py. Created empty classes and functions with comments.

--- music_module.py
+++ music_module.py
@@ -14,15 +14,15 @@ class Pattern:
         self.length = length
         self.channels = channels
         self.data = [
-            [Note() for i in range(length)] for j in range(channels)
+            [Note() for i in range(self.length)] for j in range(self.channels)
         ]
 
     def __str__(self):
         out = ''
-        for step in range(len(self.data[0])):
-            for channel in range(len(self.data)):
-                #out += '(' + str(channel) + ', ' + str(step) + '):'
-                out += str(self.data[channel][step].note)
+        for step in range(self.length):
+            for channel in range(self.channels):
+                out += '(' + str(channel) + ', ' + str(step) + '): '
+                out += str(self.data[channel][step].note) + '	'
             out += '\n'
 
         return out
@@ -43,6 +43,80 @@ class Note:
 
     def __str__(self):
         return str(self.note)
+        
+class NoteIntegerList:
+	pass
+	
+class PitchClassIntegerList:
+	pass
+	
+class OrderedNoteSet(NoteIntegerList):
+	pass
+	
+class UnorderedNoteSet(NoteIntegerList):
+	pass	
+	
+class OrderedPitchClassSet(PitchClassIntegerList):
+	pass
+	
+class UnorderedPitchClassSet(PitchClassIntegerList):
+	pass
+
+def listToBasicPattern(list):
+	'''Takes a list of notes (int) and creates a Pattern with the notes
+	using one channel and the length of the list.'''
+	return Pattern
+	
+def rhythmToBasicPattern(list):
+	'''Takes a list with 1 representing a note on, 0 representing a rest
+	or note continuation, and -1 representing a note off and create a
+	pattern with one channel and the length of the list playing the
+	chromatic scale using that rhythm.'''
+	return Pattern
+	
+def expandPattern(PatternA, multiplier):
+	'''Takes creates a new pattern whose length is equal to the input
+	pattern's length times the multiplier and copies the input to it,
+	interpolating or removing data as needed.'''
+	return PatternB
+	
+def firstNoteOnZero(Pattern):
+	'''Transposes the pattern so the first note is 0'''
+	return Pattern
+	
+def bottomNoteOnZero(Pattern):
+	'''Transposes the pattern so the bottom note is 0'''
+	return Pattern
+	
+def firstNoteOnZero(list):
+	'''Transposes the list so the first note is 0'''
+	return Pattern
+	
+def bottomNoteOnZero(list):
+	'''Transposes the list so the bottom note is 0'''
+	return Pattern
+	
+def indexToTransposition(PatternA, PatternB):
+	'''Replaces the notes in PatternA with the notes of PatternB using
+	the notes of PatternA as an index velue to choose notes from 
+	PatternB'''
+	pass
+	
+def noteToToneLoop(PatternA, PatternB):
+	'''Replaces the notes in PatternA with the notes of PatternB by 
+	choosing the next note in PatternB each time a new note is played in
+	PatternA and replacing the note in PatternA with it.'''
+	pass
+	
+def duplicateAndTranspose(PatternA, PatternB):
+	'''Transposes and duplicates pattern A for every note in pattern B.
+	'''
+	pass
+	
+def copyPattern(PatternA, PatternB, step_offset=0, channel_offset=0):
+	'''Copies pattern B to pattern A. If pattern B is longer than A it
+	is cut off'''
+	pass
 
 m = Module()
 m.AddPattern()
